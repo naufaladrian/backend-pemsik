9f6619bc2d2bfeb04dcd071e2e75ed80f54101b2
naufaladrian <naufal.adrian904@gmail.com>
2025-01-11 16:14:37 +0700

    finish??

--- elysia/index.ts
+++ elysia/index.ts
@@ -3,9 +3,11 @@ import { Elysia, t } from 'elysia'
 import { messageController } from './controllers/message'
 import { authController } from './controllers/auth'
 import { reportController } from './controllers/report'
+import cors from '@elysiajs/cors'
 
 
 export const elysiaApp = new Elysia({ prefix: '/api' })
+    .use(cors())
     .use(messageController).onError(({ code, error }) => {
         console.log(code)
         return new Response(JSON.stringify({ error: error.toString() }), { status: 500 })
